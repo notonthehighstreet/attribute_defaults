76b7db7b2492d04eda57415bb791022559f10cf0
Jonathan Lim <jonlim@notonthehighstreet.com>
2021-10-11 14:23:39 +0100

    Use sqlite

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "http://rubygems.org"
 gem 'activerecord', '~> 4.2.0'
 
 group :development, :test do
-  gem 'mysql2'
+  gem 'sqlite3', "~> 1.3.13"
   gem 'rake'
   gem 'rspec'
   gem 'rspec_junit_formatter'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path("../../lib/attribute_defaults", __FILE__)
 
 ActiveRecord::Base.configurations = YAML::load(IO.read(File.dirname(__FILE__) + "/database.yml"))
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(File.dirname(__FILE__) + "/debug.log")
-ActiveRecord::Base.establish_connection(:mysql)
+ActiveRecord::Base.establish_connection(:test)
 
 load(File.dirname(__FILE__) + "/schema.rb")
 
@@ -19,20 +19,20 @@ end
 
 class Person < ActiveRecord::Base
   belongs_to :school
-  
+
   # Include an aggregate reflection to check compatibility
   composed_of :address, :mapping => [%w(address_suburb suburb), %(address_city city)]
-  
+
   defaults :city => "Christchurch", :country => lambda { "New Zealand" }
-  
+
   default :first_name => "Sean"
-  
+
   default :last_name do
     "Fitzpatrick"
   end
-  
+
   defaults :lucky_number => lambda { 2 }, :favourite_colour => :default_favourite_colour
-  
+
   def default_favourite_colour
     last_name == "Fitzpatrick" ? "Blue" : "Red"
   end
